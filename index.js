53701190ddc22522db10a08d2c0924e99afec43b
angelo-lepore <angelo.lepore94@gmail.com>
2025-06-27 15:20:39 +0200

    Added API route to return blog posts in JSON format

--- index.js
+++ index.js
@@ -15,6 +15,45 @@ app.get("/", (req, res) => {
 
 // creiamo un array
 
-// creiamo una rotta che restituisca un oggetto json con l'array appena creata
+const post = [
+  {
+    title: "Ciambellone",
+    content: "Un dolce semplice e soffice, perfetto per la colazione.",
+    image: "images/ciambellone.jpeg",
+    tags: ["colazione", "dolce", "soffice"],
+  },
+  {
+    title: "Cracker Barbabietola",
+    content: "Snack salato a base di barbabietola.",
+    image: "images/cracker_barbabietola.jpeg",
+    tags: ["snack", "salato", "barbabietola"],
+  },
+  {
+    title: "Pane fritto dolce",
+    content:
+      "Un dolce fritto e croccante, perfetto da gustare con una spolverata di zucchero a velo.",
+    image: "images/pane_fritto_dolce.jpeg",
+    tags: ["colazione", "dolce", "fritto"],
+  },
+  {
+    title: "Pasta barbabietola",
+    content: "Un primo piatto colorato e saporito.",
+    image: "images/pasta_barbabietola.jpeg",
+    tags: ["primo", "barbabietola", "colorato"],
+  },
+  {
+    title: "Torta paesana",
+    content:
+      "Un dolce tipico della tradizione contadina, preparato con ingredienti semplici.",
+    image: "images/torta_paesana.jpeg",
+    tags: ["dolce", "tradizione", "contadino"],
+  },
+];
+
+// definiamo una rotta che restituisca un oggetto json con l'array appena creata
+
+app.get("/api/post", (req, res) => {
+  res.json(post);
+});
 
 // configuriamo gli asset statici
